24737a258297524ced96e3dcc38c2095dfff6fd5
mfl <mfl@mfl.mm>
2015-09-26 14:29:09 +0800

    bind3

--- app.py
+++ app.py
@@ -3,8 +3,8 @@
 
 import os
 from twisted.internet import reactor, protocol
-local_hostname =os.getenv("OPENSHIFT_INTERNAL_IP")
-
+local_hostname =os.getenv("OPENSHIFT_PYTHON_IP")
+local_port=os.getenv("OPENSHIFT_PYTHON_PORT")
 
 class Echo(protocol.Protocol):
     """This is just about the simplest possible protocol"""
@@ -18,7 +18,7 @@ def main():
     """This runs the protocol on port 8000"""
     factory = protocol.ServerFactory()
     factory.protocol = Echo
-    reactor.listenTCP(20000,factory)
+    reactor.listenTCP(local_port,factory,interface=local_hostname)
     reactor.run()
 
 # this only runs if the module was *not* imported
